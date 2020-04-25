1300eafbf99cff595510275839dc697a6617f07f
Vishnu Varthan <vichu@Vishnus-MacBook-Air.local>
2020-04-25 10:24:57 +0530

    Release of Version 1.0

---
+++ setup.py
@@ -0,0 +1,41 @@
+from distutils.core import setup
+from setuptools import setup,setuptools
+import os 
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme_file:
+    readme = readme_file.read()
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-picsum',
+    version='1.0',
+    url ='https://github.com/VarthanV/django-picsum.git',
+    description='Pixabay and Picsum for Django ',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    long_description=open('README.rst').read(),
+    author = "Vishnu Varthan Rao",
+    author_email="vishnulatha006@gmail.com",
+    install_requires=[
+        "Django > 2.0",
+    ],
+    license="MIT License",
+    zip_safe=False,
+    keywords='picsum,pixabay,django-pics,django image,django template',
+     classifiers=[
+        'Development Status :: Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+    ],
+
+)
